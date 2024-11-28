6104a7c5fba9db5ffcd1ed8dca194e47aa373e0f
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-28 04:36:59 +0100

    Find NegativeSum, min element and index of max element in array

--- Sana03/Program.cs
+++ Sana03/Program.cs
@@ -1,19 +1,38 @@
-﻿double[] array;
-double negativeSum = 0;
+﻿int[] array;
+int negativeSum = 0;
 
 Console.Write("Введіть к-ть елементів масиву: ");
 if(int.TryParse(Console.ReadLine(), out int n));
-array = new double[n];
+
+array = new int[n];
+int min = array[0];
+int max = array[0];
+int maxIndex = 0;
+int maxAbs = array[0];
 
 Console.WriteLine("Введіть значення елементів масиву: ");
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"[{i}] ", array[i]);
-    array[i] = double.Parse(Console.ReadLine());
+    array[i] = int.Parse(Console.ReadLine());
     if (array[i] < 0)
     {
         negativeSum += array[i];
     }
 }
 
+for (int i = 1; i < array.Length; i++)
+{
+    if(array[i]>max) {
+        max = array[i];
+        maxAbs = Math.Abs(max);
+        maxIndex = i;
+    } 
+    if(array[i]<min) {
+        min = array[i];
+    }
+}
+
 Console.WriteLine($"Сума відʼємних елементів: {negativeSum}");
+Console.WriteLine($"Мінімальний елемент масиву: {min}");
+Console.WriteLine($"Індекс максимального елементу масиву: {maxIndex}");
