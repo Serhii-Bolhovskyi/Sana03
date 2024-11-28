6c051f9a1d606559ab4894f3380e0ba63d5dad8c
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-28 03:32:27 +0100

    Solution for first exercise

--- Sana03/Program.cs
+++ Sana03/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("First commit");
+﻿double[] array;
+double negativeSum = 0;
+
+Console.Write("Введіть к-ть елементів масиву: ");
+if(int.TryParse(Console.ReadLine(), out int n));
+array = new double[n];
+
+Console.WriteLine("Введіть значення елементів масиву: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"[{i}] ", array[i]);
+    array[i] = double.Parse(Console.ReadLine());
+    if (array[i] < 0)
+    {
+        negativeSum += array[i];
+    }
+}
+
+Console.WriteLine($"Сума відʼємних елементів: {negativeSum}");
